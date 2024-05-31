2f4fbc14cea42c66007bb25ac7ecbeffeb1779bd
euberrino <eberrino@itba.edu.ar>
2024-05-31 03:01:57 +0000

    mice

--- src/workflow-01/509_workflow_base.r
+++ src/workflow-01/509_workflow_base.r
@@ -386,7 +386,8 @@ wf_sept <- function( pnombrewf )
   param_local <- exp_wf_init( pnombrewf ) # linea fija
 
   DT_incorporar_dataset_competencia2024()
-  CA_catastrophe_base( metodo="MachineLearning")
+  A_catastrophe_base( metodo="MachineLearning")
+  CA_catastrophe_base( metodo="MICE")
   FEintra_base()
   DR_drifting_base(metodo="rank_cero_fijo")
   FEhist_base()
